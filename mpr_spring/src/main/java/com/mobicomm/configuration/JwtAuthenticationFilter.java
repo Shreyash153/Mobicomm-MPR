135ea03a3df87c30e81b1cd47d5c869944a37216
Shreyash153 <shreyashmadake153@gmail.com>
2024-08-05 15:47:53 +0530

    test push

--- mpr_spring/src/main/java/com/mobicomm/configuration/JwtAuthenticationFilter.java
+++ mpr_spring/src/main/java/com/mobicomm/configuration/JwtAuthenticationFilter.java
@@ -4,10 +4,7 @@ import java.io.IOException;
 
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-<<<<<<< HEAD
-=======
 import org.springframework.security.core.context.SecurityContext;
->>>>>>> 6c7159eb4e592d2e359281dd809e21b0494a4f73
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
@@ -21,47 +18,10 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-<<<<<<< HEAD
-
-=======
->>>>>>> 6c7159eb4e592d2e359281dd809e21b0494a4f73
-import lombok.RequiredArgsConstructor;
+// import lombok.RequiredArgsConstructor;
 
 @Component
-@RequiredArgsConstructor
-<<<<<<< HEAD
-public class JwtAuthenticationFilter extends OncePerRequestFilter {
-    
-    private final JWTUtil jwtUtil;
-    private final UserService userService;
-
-    @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
-            throws ServletException, IOException {
-        
-        final String authHeader = request.getHeader("Authorization");
-        
-        if (StringUtils.isEmpty(authHeader) || !StringUtils.startsWith(authHeader, "Bearer ")) {
-            filterChain.doFilter(request, response);
-            return;
-        }
-        
-        final String jwt = authHeader.substring(7);
-        final String userEmail = jwtUtil.extractUserName(jwt);
-        
-        if (StringUtils.isNotEmpty(userEmail) && SecurityContextHolder.getContext().getAuthentication() == null) {
-            UserDetails userDetails = userService.userDetailsService().loadUserByUsername(userEmail);
-            if (jwtUtil.isTokenValid(jwt, userDetails)) {
-                UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
-                        userDetails, null, userDetails.getAuthorities());
-                authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-                SecurityContextHolder.getContext().setAuthentication(authToken);
-            }
-        }
-        
-        filterChain.doFilter(request, response);
-    }
-=======
+// @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter{
 	
 	private final JWTUtil jwtUtil;
@@ -103,5 +63,4 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter{
 	}
 	
 
->>>>>>> 6c7159eb4e592d2e359281dd809e21b0494a4f73
 }
